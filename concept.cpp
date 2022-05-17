0f97ab2b099e41234bd47e2322f3f1ec29e32fb6
BHAGYAshriTAMRAKAR <bt7355711982@gmail.com>
2022-05-17 12:11:21 +0530

    second commit

---
+++ concept.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int bs(vector<int>v,int n, int element)
+{
+   int left =0 ; // v[0]
+   int right = n;   //v[n] size of array
+    while(left<=right)
+    {
+        int mid= left+(right-1)/2;
+
+        if(element==v[mid]){ return mid;}
+        else if(element<v[mid]){ right=mid-1;}
+        else { left=mid+1;}
+    }
+    return -1;
+}
+int main(){
+    int n; // size of array
+    cin>>n;
+
+    vector<int>v(n);
+   for (int i=0;i<n;i++){
+        cin>>v[i];}
+    int element,result;
+    cin>>element;
+    result =  bs(v,n,element);
+cout <<"Value at index "<< result  << ' ';
+    
+}
